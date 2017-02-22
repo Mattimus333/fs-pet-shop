ac7f0b6b6210f25fdd243f9f29ae62a3ce9142db
Matt <mmuhr88@gmail.com>
2017-02-22 15:54:42 -0800

    finished the filesystem

--- pets.js
+++ pets.js
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env node
 
 'use strict';
 var fs = require('fs');
@@ -10,43 +10,130 @@ var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
 if (cmd === 'read') {
-  fs.readFile(petsPath, 'utf8', function(err, data){
-    if (err){
-      throw err;
-    }
+    fs.readFile(petsPath, 'utf8', function(err, data) {
+        if (err) {
+            throw err;
+        }
 
-    var pets = JSON.parse(data);
-    console.log(pets);
-  });
+        var pets = JSON.parse(data);
+        var index = process.argv[3];
+
+        if (index) {
+            if (index >= pets.length || index < 0) {
+                console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
+                process.exit(1);
+            } else {
+                console.log(pets[index]);
+            }
+        } else {
+            console.log(pets);
+        }
+    });
+} else if (cmd === 'create') {
+    fs.readFile(petsPath, 'utf8', function(readErr, data) {
+        if (readErr) {
+            throw readErr;
+        }
+
+        var pets = JSON.parse(data);
+        var age = process.argv[3];
+        var kind = process.argv[4];
+        var name = process.argv[5];
+
+        if (age && kind && name) {
+          let newPet = {};
+          newPet.age = parseInt(age);
+          newPet.kind = kind;
+          newPet.name = name;
+          pets.push(newPet);
+          console.log(newPet);
+        }
+        else {
+          console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+          process.exit(1);
+        }
+
+        var petsJSON = JSON.stringify(pets);
+
+        fs.writeFile(petsPath, petsJSON, function(writeErr) {
+            if (writeErr) {
+                throw writeErr;
+            }
+        });
+    });
 }
-else if (cmd === 'create'){
-  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+else if (cmd === 'update') {
+  fs.readFile(petsPath, 'utf-8', function(readErr, data){
     if (readErr) {
       throw readErr;
     }
 
     var pets = JSON.parse(data);
-    var pet = process.argv[3];
+    var index = process.argv[3];
+    var age = process.argv[4];
+    var kind = process.argv[5];
+    var name = process.argv[6];
 
-    if(!pet) {
-      console.log(`Usage: ${node} ${file} ${cmd} PET`);
+    if (index && age && kind && name) {
+      if (index >= pets.length || index < 0) {
+          console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
+      }
+      else {
+        let newPet = {};
+        newPet.age = parseInt(age);
+        newPet.kind = kind;
+        newPet.name = name;
+        pets[index] = newPet;
+        console.log(newPet);
+      }
+    }
+    else {
+      console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
       process.exit(1);
     }
-
-    pets.push(pet);
 
     var petsJSON = JSON.stringify(pets);
 
     fs.writeFile(petsPath, petsJSON, function(writeErr) {
-      if (writeErr) {
-        throw writeErr;
-      }
-
-      console.log(pet);
+        if (writeErr) {
+            throw writeErr;
+        }
     });
   });
 }
-else{
-  console.log(`Usage: ${node}, ${file}, read`);
-  process.exit(1);
+else if (cmd === 'destroy') {
+  fs.readFile(petsPath, 'utf-8', function(readErr, data){
+    if (readErr) {
+      throw readErr;
+    }
+
+    let pets = JSON.parse(data);
+    let index = process.argv[3];
+
+    if (index) {
+      if (index >= pets.length || index < 0) {
+          console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
+          process.exit(1);
+      }
+      else {
+        console.log(pets.splice(index, 1)[0]);
+      }
+    }
+    else {
+      console.error(`Usage: ${node} ${file} ${cmd} INDEX`);
+      process.exit(1);
+    }
+
+    var petsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, petsJSON, function(writeErr) {
+        if (writeErr) {
+            throw writeErr;
+        }
+    });
+  });
+}
+else {
+    console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
+    process.exit(1);
 }
