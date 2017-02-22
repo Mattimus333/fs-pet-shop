d9c9dccc18231c5334f8a4037e0560ae94c30b36
Matt <mmuhr88@gmail.com>
2017-02-22 13:26:40 -0800

    added the create subcommand

--- pets.js
+++ pets.js
@@ -19,6 +19,33 @@ if (cmd === 'read') {
     console.log(pets);
   });
 }
+else if (cmd === 'create'){
+  fs.readFile(petsPath, 'utf8', function(readErr, data) {
+    if (readErr) {
+      throw readErr;
+    }
+
+    var pets = JSON.parse(data);
+    var pet = process.argv[3];
+
+    if(!pet) {
+      console.log(`Usage: ${node} ${file} ${cmd} PET`);
+      process.exit(1);
+    }
+
+    pets.push(pet);
+
+    var petsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsPath, petsJSON, function(writeErr) {
+      if (writeErr) {
+        throw writeErr;
+      }
+
+      console.log(pet);
+    });
+  });
+}
 else{
   console.log(`Usage: ${node}, ${file}, read`);
   process.exit(1);
