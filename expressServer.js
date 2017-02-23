83d0c4aff2e26d9eeff09ea16fe91f28b65bd9df
Matt <mmuhr88@gmail.com>
2017-02-23 09:08:39 -0800

    finished

--- expressServer.js
+++ expressServer.js
@@ -6,6 +6,10 @@ var petsPath = path.join(__dirname, 'pets.json');
 
 var express = require('express');
 var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 var port = process.env.PORT || 8000;
 
 app.disable('x-powered-by');
@@ -23,22 +27,57 @@ app.get('/pets', function(req, res){
   });
 });
 
-app.get('/pets/:id', function(req, res) {
+app.get('/pets/:index', function(req, res) {
   fs.readFile(petsPath, 'utf8', function(err, petsJSON){
     if (err) {
       console.error(err.stack);
       return res.sendStatus(500);
     }
 
-    var id = Number.parseInt(req.params.id);
+    var index = Number.parseInt(req.params.index);
     var pets = JSON.parse(petsJSON);
 
-    if (id < 0 || id >= pets.length || Number.isNaN(id)){
+    if (index < 0 || index >= pets.length || Number.isNaN(index)){
       return res.sendStatus(404);
     }
 
     res.set('Content-Type', 'application/json');
-    res.send(pets[id]);
+    res.send(pets[index]);
+  });
+});
+
+app.post('/pets', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var age = parseInt(req.body.age);
+    var kind = req.body.kind;
+    var name = req.body.name;
+    var pets = JSON.parse(petsJSON);
+
+    if (age && kind && name && !Number.isNaN(age)) {
+      let newPet = {};
+      newPet.age = age;
+      newPet.kind = kind;
+      newPet.name = name;
+      pets.push(newPet);
+      res.set('Content-Type', 'application/json');
+      res.send(newPet);
+    }
+    else{
+      res.sendStatus(400);
+    }
+
+    var petsJSON = JSON.stringify(pets);
+    fs.writeFile(petsPath, petsJSON, function(writeErr) {
+        if (writeErr) {
+            console.log(writeErr.stack);
+        }
+    });
+
   });
 });
 
