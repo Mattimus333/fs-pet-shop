6249f73f1ae32b765936f67f66a264f81e254f14
Matt <mmuhr88@gmail.com>
2017-02-23 14:38:15 -0800

    got green checks!

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -8,6 +8,7 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+var morgan = require('morgan');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
@@ -15,7 +16,7 @@ var port = process.env.PORT || 8000;
 
 app.disable('x-powered-by');
 
-app.use(morgan('common'));
+// app.use(morgan('common'));
 
 app.get('/pets', function(req, res){
   fs.readFile(petsPath, 'utf8', function(err, petsJSON){
@@ -82,6 +83,74 @@ app.post('/pets', function(req, res) {
     });
 
   });
+});
+
+app.patch('/pets/:index', function(req, res){
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var index = Number.parseInt(req.params.index);
+    var age = parseInt(req.body.age);
+    var kind = req.body.kind;
+    var name = req.body.name;
+    var pets = JSON.parse(petsJSON);
+
+    if (index < 0 || index >= pets.length || Number.isNaN(index)){
+      return res.sendStatus(404);
+   } else {
+     if (kind) {
+       pets[index].kind = kind;
+     }
+     if (age) {
+       pets[index].age = age;
+     }
+     if (name) {
+       pets[index].name = name;
+     }
+     res.set('Content-Type', 'application/json');
+     res.send(pets[index]);
+   }
+
+    var petsJSON = JSON.stringify(pets);
+    fs.writeFile(petsPath, petsJSON, function(writeErr) {
+        if (writeErr) {
+            console.log(writeErr.stack);
+            res.sendStatus(500);
+        }
+    });
+
+  });
+});
+
+app.delete('/pets/:index', function(req, res){
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON){
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var index = Number.parseInt(req.params.index);
+    var pets = JSON.parse(petsJSON);
+
+    if (index < 0 || index >= pets.length || Number.isNaN(index)){
+      return res.sendStatus(404);
+   } else {
+     res.set('Content-Type', 'application/json');
+     res.send(pets.splice(index, 1)[0]);
+   }
+
+   var petsJSON = JSON.stringify(pets);
+   fs.writeFile(petsPath, petsJSON, function(writeErr) {
+       if (writeErr) {
+           console.log(writeErr.stack);
+           res.sendStatus(500);
+       }
+   });
+
+ });
 });
 
 app.use(function(req, res) {
